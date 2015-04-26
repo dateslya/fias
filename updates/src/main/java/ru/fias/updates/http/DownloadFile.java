eabaa03f639573545dbc5dce12a4597d09aa7531
Dmitry Teslya <d.a.teslya@gmail.com>
2015-04-27 02:49:46 +0400

    Логирование через slf4j. commons-io больше не используется

--- updates/src/main/java/ru/fias/updates/http/DownloadFile.java
+++ updates/src/main/java/ru/fias/updates/http/DownloadFile.java
@@ -1,6 +1,7 @@
 package ru.fias.updates.http;
 
-import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
 import java.io.File;
@@ -10,14 +11,16 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.logging.Logger;
 
 /**
  * Created by Dmitry Teslya on 25.04.2015.
  */
 public class DownloadFile {
 
-    private static final Logger log = Logger.getLogger(DownloadFile.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(DownloadFile.class.getName());
+
+    private static final int BUFFER_SIZE = 1024 * 4;
+    private static final int END_OF_STREAM = -1;
 
     public static final int CONNECTION_TIMEOUT = 30 * 1000;
     public static final int READ_TIMEOUT = 10 * 1000;
@@ -25,10 +28,10 @@ public class DownloadFile {
     public void download(URL source, File target) throws IOException {
         long downloaded = target.exists() ? target.length() : 0;
         HttpURLConnection connection = (HttpURLConnection) source.openConnection();
-        log.info("URL: " + source);
+        log.info("URL: {}", source);
         if (connection.getContentLength() == downloaded) {
             connection.disconnect();
-            log.info(source + " is downloaded");
+            log.info("{} is downloaded", source);
             return;
         } else {
             connection.disconnect();
@@ -36,19 +39,23 @@ public class DownloadFile {
             connection.setConnectTimeout(CONNECTION_TIMEOUT);
             connection.setReadTimeout(READ_TIMEOUT);
             connection.setRequestProperty("Range", "bytes=" + downloaded + "-");
-            log.info("Content-Type: " + connection.getContentType());
-            log.info("Content-Length: " + connection.getContentLength());
-            log.info("Range: bytes=" + downloaded + "-");
+            log.info("Content-Type: {}", connection.getContentType());
+            log.info("Content-Length: {}", connection.getContentLength());
+            log.info("Range: bytes={}-", downloaded);
         }
-        InputStream inputStream = null;
-        OutputStream outputStream = null;
+        InputStream input = null;
+        OutputStream output = null;
         try {
-            inputStream = connection.getInputStream();
-            outputStream = new FileOutputStream(target, downloaded != 0);
-            IOUtils.copyLarge(inputStream, outputStream);
+            input = connection.getInputStream();
+            output = new FileOutputStream(target, downloaded != 0);
+            int length = 0;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while ((length = input.read(buffer)) != END_OF_STREAM) {
+                output.write(buffer, 0, length);
+            }
         } finally {
-            closeStream(inputStream);
-            closeStream(outputStream);
+            closeStream(input);
+            closeStream(output);
             connection.disconnect();
         }
     }
